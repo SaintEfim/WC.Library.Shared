62407b2d620be4831530e112884085d62813664b
SaintEfim <saintefim2@gmail.com>
2024-04-10 12:42:56 +0300

    create new library

---
+++ WC.Library.Shared/Exceptions/NotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace WC.Library.Shared.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException()
+    {
+    }
+
+    public NotFoundException(string message) : base(message)
+    {
+    }
+
+    public string Title = "User Not Found";
+    public int StatusCode = (int)HttpStatusCode.NotFound;
+}
