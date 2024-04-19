805d44507b4fe63ef397d3d539d411103d09cce5
SaintEfim <saintefim2@gmail.com>
2024-04-19 10:33:39 +0300

    added new const

---
+++ src/WC.Library.Shared/Constants/ErrorSettingsConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace WC.Library.Shared.Constants;
+
+public class ErrorSettingsConstants
+{
+    public const string ErrorUrlType = "https://datatracker.ietf.org/doc/html/rfc9110#section-15.5.1";
+
+    public const string DefaultTitle = "One or more validation errors occurred.";
+}
