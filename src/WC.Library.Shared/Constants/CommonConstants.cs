dd96003c2072163422afce22317c5b3fb8a0bb21
SaintEfim <saintefim2@gmail.com>
2024-06-14 16:55:29 +0300

    added new constant

---
+++ src/WC.Library.Shared/Constants/CommonConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace WC.Library.Shared.Constants;
+
+public class CommonConstants
+{
+    public const int GenericPasswordMinLength = 8;
+    
+    public const int GenericPasswordMaxLength = 64;
+    
+    public const int GenericEmailMinLength = 8;
+    
+    public const int GenericEmailMaxLength = 64;
+}
