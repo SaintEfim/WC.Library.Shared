0775a01ad08d78270f392cc55925a42a65be1318
SaintEfim <saintefim2@gmail.com>
2024-06-15 16:53:12 +0300

    added new const

--- src/WC.Library.Shared/Constants/CommonConstants.cs
+++ src/WC.Library.Shared/Constants/CommonConstants.cs
@@ -3,10 +3,12 @@
 public class CommonConstants
 {
     public const int GenericPasswordMinLength = 8;
-    
+
     public const int GenericPasswordMaxLength = 64;
-    
+
     public const int GenericEmailMinLength = 8;
-    
+
     public const int GenericEmailMaxLength = 64;
+
+    public const char GenericAtSymbol = '@';
 }
