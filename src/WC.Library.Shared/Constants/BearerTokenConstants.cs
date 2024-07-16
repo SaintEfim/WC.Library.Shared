adb3f303a7e6e296831d114c70675179cfbd2f6f
SaintEfim <saintefim2@gmail.com>
2024-07-16 15:36:12 +0300

    update

--- src/WC.Library.Shared/Constants/BearerTokenConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WC.Library.Shared.Constants;
-
-public static class BearerTokenConstants
-{
-    public const string TokenType = "Bearer";
-
-    public const string DescriptionToken = "Jwt Authorization Bearer";
-}
--- src/WC.Library.Shared/Constants/CommonConstants.cs
+++ src/WC.Library.Shared/Constants/CommonConstants.cs
@@ -2,13 +2,5 @@
 
 public class CommonConstants
 {
-    public const int GenericPasswordMinLength = 8;
-
-    public const int GenericPasswordMaxLength = 64;
-
-    public const int GenericEmailMinLength = 8;
-
-    public const int GenericEmailMaxLength = 64;
-
     public const char GenericAtSymbol = '@';
 }
